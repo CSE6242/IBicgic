0b7995d1c8f07a397e31508f38005bdeade90121
scinart <akubeej@gmail.com>
2018-11-29 07:34:42 -0500

    update piechart.js

--- src/overview/components/piechart.js
+++ src/overview/components/piechart.js
@@ -4,46 +4,13 @@ import * as dc from 'dc';
 import * as crossfilter from 'crossfilter';
 import axios from 'axios';
 
-export class PieChart extends Component {
-
+export class PieChartUrl extends Component {
     constructor(props){
         super(props);
 	this.id = props.id;
-    }
-
-    update(data)
-    {
-	let chart = dc.pieChart(this.id);
-	let ndx = crossfilter(this.props.data);
-	let runDimension  = ndx.dimension(function(d) {return d.userType;});
-	let speedSumGroup = runDimension.group().reduceSum(function(d) {return d["sum(tripDuration)"];});
-
-	chart
-	    .width(768)
-	    .height(480)
-	    .slicesCap(4)
-	    .innerRadius(100)
-	    .dimension(runDimension)
-	    .group(speedSumGroup)
-	    .legend(dc.legend())
-	    .on('pretransition', function(chart) {
-		chart.selectAll('text.pie-slice').text(function(d) {
-		    return d.data.key + ' ' + dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2*Math.PI) * 100) + '%';
-		});
-	    });
-	chart.render();
-    }
-    
-    render () {
-	this.update(this.props.data);
-	return null;
-    }
-}
-
-export class PieChartUrlUserType extends Component {
-    constructor(props){
-        super(props);
-	this.id = props.id;
+	this.config = props.config;
+	if (!this.config.slicesCap)
+	    this.config.slicesCap = 20;
     }
 
     update(data)
@@ -52,22 +19,26 @@ export class PieChartUrlUserType extends Component {
 	console.log(data);
 	let chart = dc.pieChart(this.id);
 	let ndx = crossfilter(data);
-	let runDimension  = ndx.dimension(function(d) {return d.userType;});
-	let speedSumGroup = runDimension.group().reduceSum(function(d) {return d["sumtrip"];});
+	let config = this.config;
+	let runDimension  = ndx.dimension(config.key);
+	let speedSumGroup = runDimension.group().reduceSum(config.value);
 
 	chart
 	    .width(768)
 	    .height(480)
-	    .slicesCap(6)
+	    .slicesCap(config.slicesCap)
 	    .innerRadius(100)
+	    .emptyTitle("empty")
 	    .dimension(runDimension)
 	    .group(speedSumGroup)
 	    .legend(dc.legend())
 	    .on('pretransition', function(chart) {
-		chart.selectAll('text.pie-slice').text(function(d) {
+	    	chart.selectAll('text.pie-slice').text(function(d) {
+		    console.log("click!", d, "my id is ", this.id);
 		    return d.data.key + ' ' + dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2*Math.PI) * 100) + '%';
-		});
-	    });
+	    	});
+	    })
+	;
 	chart.render();
     }
 
@@ -79,84 +50,4 @@ export class PieChartUrlUserType extends Component {
     }    
 }
 
-export class PieChartUrlUserAge extends Component {
-    constructor(props){
-        super(props);
-	this.id = props.id;
-    }
-
-    update(data)
-    {
-	console.log("this.id is " + this.id);
-	console.log(data);
-	let chart = dc.pieChart(this.id);
-	let ndx = crossfilter(data);
-	let runDimension  = ndx.dimension(function(d) {return d.age;});
-	let speedSumGroup = runDimension.group().reduceSum(function(d) {return d["countUTP"];});
-
-	chart
-	    .width(768)
-	    .height(480)
-	    .slicesCap(10)
-	    .innerRadius(100)
-	    .dimension(runDimension)
-	    .group(speedSumGroup)
-	    .legend(dc.legend())
-	    .on('pretransition', function(chart) {
-		chart.selectAll('text.pie-slice').text(function(d) {
-		    return d.data.key + ' ' + dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2*Math.PI) * 100) + '%';
-		});
-	    });
-	chart.render();
-    }
-
-    render () {
-	axios.get(this.props.dataurl).then((res)=>{
-	    this.update(res.data);
-	});
-	return null;
-    }    
-}
-
-export class PieChartUrlUserGender extends Component {
-    constructor(props){
-        super(props);
-	this.id = props.id;
-    }
-
-    update(data)
-    {
-	console.log("this.id is " + this.id);
-	console.log(data);
-	let chart = dc.pieChart(this.id);
-	let ndx = crossfilter(data);
-	let runDimension  = ndx.dimension(function(d) {return d.gender===1?"Male":"Female";});
-	let speedSumGroup = runDimension.group().reduceSum(function(d) {return d["sumtrip"];});
-
-	chart
-	    .width(768)
-	    .height(480)
-	    .slicesCap(10)
-	    .innerRadius(100)
-	    .dimension(runDimension)
-	    .group(speedSumGroup)
-	    .legend(dc.legend())
-	    .on('pretransition', function(chart) {
-		chart.selectAll('text.pie-slice').text(function(d) {
-		    return d.data.key + ' ' + dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2*Math.PI) * 100) + '%';
-		});
-	    });
-	chart.render();
-    }
-
-    render () {
-	axios.get(this.props.dataurl).then((res)=>{
-	    this.update(res.data);
-	});
-	return null;
-    }    
-}
-
-
-
-export default PieChart;
+export default PieChartUrl;
--- src/overview/container.js
+++ src/overview/container.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import BrushChart from './components/brushchart';
-import PieChart, {PieChartUrlUserType, PieChartUrlUserAge, PieChartUrlUserGender} from './components/piechart';
+import PieChartUrl, {PieChartUrlUserType, PieChartUrlUserAge, PieChartUrlUserGender} from './components/piechart';
 import MoveChart from './components/movechart';
 import BarChart, { RowChart } from './components/barchart';
 import TablePopular from './components/table';
@@ -31,6 +31,7 @@ class ChartContainer extends Component {
         return (
 		<div className="brush-chart-container" data={this.state.data}>
                 <div className='brush-chart' />
+		<div className='pie-chart' id="usertype2" />
 		<div className='pie-chart' id="usertype" />
 		<div className='pie-chart' id="userage" />
 		<div className='pie-chart' id="usergender" />
@@ -40,9 +41,12 @@ class ChartContainer extends Component {
 		<div className='barchart' id="barchart1" />
 		<div className='barchart' id="barchart2" />
                 <BrushChart data={this.state.data}/>
-		<PieChartUrlUserType dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=usertype" id="#usertype"/>
-		<PieChartUrlUserAge dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=age" id="#userage"/>
-		<PieChartUrlUserGender dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=gender" id="#usergender"/>
+		<PieChartUrl dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=usertype"
+	    id="#usertype2" config={{key: function(d){return d["userType"];}, value: function(d){return d["sumtrip"];}, slicesCap:8}}/>
+		<PieChartUrl dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=age"
+	    id="#userage" config={{key: function(d){return d["age"];}, value: function(d){return d["countUTP"];}}} />
+		<PieChartUrl dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=gender"
+	    id="#usergender" config={{key: function(d) {return d.gender===1?"Male":"Female";}, value: function(d) {return d["sumtrip"];}}} />
 		<TablePopular dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=hotstation" id="#tablepopular"/>
 		<MoveChart dataurl='https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/all-use?divide_by=day' id1="#movechart" id2="#volumechart"/>
 		<BarChart dataurl='https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=gender' id="#barchart1"/>
