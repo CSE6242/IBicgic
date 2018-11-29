30e9a3720c994d57d3997c3af4390d577162cb13
scinart <akubeej@gmail.com>
2018-11-28 21:23:29 -0500

    静态Barchart完成

--- src/chart/container.js
+++ src/chart/container.js
@@ -10,13 +10,19 @@ class BarChartContainer extends Component {
     }
 
     componentDidMount() {
+	var miniHeight = 60;
 	d3.select("body").append("svg").attr("width","960").attr("height","500");
+
 	var svg = d3.select("svg"),
 	    margin = {top: 20, right: 20, bottom: 110, left: 40},
 	    margin2 = {top: 430, right: 20, bottom: 30, left: 40},
 	    width = +svg.attr("width") - margin.left - margin.right,
 	    height = +svg.attr("height") - margin.top - margin.bottom,
 	    height2 = +svg.attr("height") - margin2.top - margin2.bottom;
+
+	var barsGroup = svg.append('g')
+	    .attr("class","barsGroup")
+	    .attr("transform","translate(" + margin.left + "," + margin.top + ")");
 
 	var x = d3.scaleTime().range([0, width]),
 	    x2 = d3.scaleTime().range([0, width]),
@@ -26,8 +32,6 @@ class BarChartContainer extends Component {
 	var xAxis = d3.axisBottom(x),
 	    xAxis2 = d3.axisBottom(x2),
 	    yAxis = d3.axisLeft(y);
-
-	var parseDate = d3.timeParse("%b %Y");
 
 	var brush = d3.brushX()
 	    .extent([[0, 0], [width, height2]])
@@ -66,7 +70,7 @@ class BarChartContainer extends Component {
 	    .attr("class", "context")
 	    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
-	let parsed_data = d3.csvParse(this.props.data, function (d) { d.date = parseDate(d.date); d.price = +d.price; return d; });
+	let parsed_data = d3.csvParse(this.props.data, function (d) { d.date = d3.timeParse("%b %Y")(d.date); d.price = +d.price; d.score = d.price; return d; });
 	function brushed() {
 	    if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
 	    var s = d3.event.selection || x2.range();
@@ -87,9 +91,83 @@ class BarChartContainer extends Component {
 	    context.select(".brush").call(brush.move, x.range().map(t.invertX, t));
 	}
 
-	function f(data) {
-	    console.log("tt");
+	function update (grp, data, main) {
+	    //grp.selectAll("rect").data(data, function (d) { return d.key; })
+	    grp.selectAll("rect").data(data)//, function (d) { return d.key; })
+		.attr("x", function (d, i) {
+		    return x(i);
+		})
+		.attr("width", function (d) {
+		    return width;
+		})
+		.attr("y", function (d){
+		    return main ? y(d.score) : 0;
+		})
+		.attr("height", function (d) {
+		    return main ? height - y(d.score) : miniHeight;
+		});
+	}
+
+	function enter (grp, data, main) {
+	    grp.selectAll("rect").data(data, function (d) {
+		return d.key;
+	    })
+		.enter()
+		.append("rect")
+		.attr("x", function (d, i) {
+		    return x(i);
+		})
+		.attr("width", function(d) {
+		    return width;
+		})
+		.attr("y", function (d){
+		    return main ? y(d.score) : 0;
+		})
+		.attr("height", function (d) {
+		    return main ? height - y(d.score) : miniHeight;
+		})
+		.attr("fill", function (d, i){
+		    var deg = d.key * 10;
+		    return "hsl(" + deg + ", 50%, 50%)";
+		})
+		.attr("opacity", function () {
+		    return 1;
+		});
+	}
+
+	function exit (grp, data) {
+	    grp.selectAll("rect").data(data, function (d) {
+		return d.key;
+	    }).exit()
+		.remove();
+	}
+
+	function updateBars (data) {
+	    console.log("nnn");
 	    console.log(data);
+	    x.domain(d3.range(data.length));
+	    y.domain([0, d3.max(data, function(d) { return d.score;})]);
+
+	    /* Update */
+	    update(barsGroup, data, true);
+
+	    /* Enter… */
+	    enter(barsGroup, data, true);
+
+	    /* Exit */
+	    exit(barsGroup, data);
+
+	    /* Call the Y axis to adjust it to the new scale */
+	    svg.select(".outer-wrapper .chart .y")
+		.transition()
+		.duration(10)
+		.call(yAxis);
+	}
+
+	//enter(miniGroup, topData, false);
+	// updateBars(parsed_data);
+	
+	function f(data) {
 	    x.domain(d3.extent(data, function(d) { return d.date; }));
 	    y.domain([0, d3.max(data, function(d) { return d.price; })]);
 	    x2.domain(x.domain());
@@ -148,11 +226,380 @@ class BarChartContainer extends Component {
 
   	    this.focus.select("path").datum(this.props.data);
 	}
-	console.log("yyy");
         return (
             <div>Hello world chart !!</div>
         );
     }
+}
+
+class BarChart2 extends Component {
+
+    constructor(props) {
+	super (props);
+    }
+
+    componentDidMount() {
+
+	d3.select("body").append("div").attr("class","outer-wrapper")
+	    .append("div").attr("class","chart")
+	    .append("div").attr("class","tool-tip");
+
+	/*	Margin, Width and height */
+	var margin = {top: 20, right: 20, bottom: 30, left: 50, mid: 20};
+	var width = d3.select('.outer-wrapper').node().getBoundingClientRect().width - margin.left - margin.right;
+	var miniHeight = 60;
+	var height = 600 - margin.top - margin.mid - miniHeight - margin.bottom;
+
+	var topData = [];
+
+	for (var i = 0; i < 100; i++) {
+	    var my_object = {};
+	    my_object.key = i;
+	    my_object.score = Math.floor(Math.random() * 600);
+	    topData.push(my_object);
+	}  
+
+	var selected;
+
+	var svg = d3.select(".outer-wrapper .chart").append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.mid + miniHeight + margin.bottom);
+
+	var barsGroup = svg.append('g')
+	    .attr("class","barsGroup")
+	    .attr("transform","translate(" + margin.left + "," + margin.top + ")");
+
+	var miniGroup = svg.append('g')
+	    .attr("class","miniGroup")
+	    .attr("transform","translate(" + margin.left + "," + (margin.top + height + margin.mid) + ")");
+
+	var brushGroup = svg.append('g')
+	    .attr("class","brushGroup")
+	    .attr("transform","translate(" + margin.left + "," + (margin.top + height + margin.mid) + ")");
+
+	/*	Scales */
+	var axisRange = d3.range(topData.length);
+
+	axisRange.shift();
+
+	axisRange.push((axisRange[axisRange.length - 1] + 1));
+
+	var yScale = d3.scaleLinear()
+	    .range([height, 0])
+	    .domain([0, d3.max(topData, function(d) { 
+		return d.score;
+	    })]);
+
+	//var xScale = d3.scaleBand([width,0], 0.4, 0).domain(d3.range(topData.length));
+	//var xScaleBrush = d3.scaleBand([width, 0], 0.4, 0).domain(d3.range(topData.length));
+	//var xScaleAxis = d3.scaleBand([width, 0], 0.4, 0).domain(axisRange);
+	var xScale      = d3.scaleBand().domain(d3.range(topData.length)).range([0,width]);
+	var xScaleBrush = d3.scaleBand().domain(d3.range(topData.length)).range([0,width]);
+	var xScaleAxis  = d3.scaleBand().domain(axisRange).range([0,width]);
+
+	/*	Define y axis */
+	var yAxis = d3.axisLeft()
+	    .scale(yScale)
+	    .tickSize(-width, -width)
+	    .ticks(4);
+
+	/* Define y axis */
+	var xAxis = d3.axisBottom()
+	    .scale(xScaleAxis)
+	    .tickSize(3,0)
+	    .tickValues([1,10,20,30,40,50,60,70,80,90,100]);
+
+
+	/*	Prepare the y axis but do not call .call(xAxis) yet */
+	svg.append("g")
+	    .attr("class", "y axis")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+	    .append("g")
+	    .attr("class", "axisLabel")
+	    .append("text")
+	    .attr("transform", "translate(" + -(margin.left * 0.8) + "," + (height/2) + "), rotate(-90)")
+	    .style("text-anchor", "middle")
+	    .text("Score");
+
+	/* Prepare the x axis */
+	svg.append("g")
+	    .attr("class","x axis")
+	    .attr("transform", "translate(" + margin.left + "," + (margin.top + height + margin.mid + miniHeight) + ")" )
+	    .call(xAxis)
+	    .append("g")
+	    .attr("class", "axisLabel")
+	    .append("text")
+	    .attr("transform", "translate(" + (width/2) + "," + margin.bottom + ")")
+	    .style("text-anchor", "middle") 
+	    .text("Order");
+
+	/* brush */
+	var brush = d3.brushX()
+	    .extent([0, width])
+	    .on("brush", display);
+
+	brushGroup.append("g")
+	    .attr("class", "brush")
+	    .call(brush)
+	    .selectAll("rect")
+	    .attr("opacity", 0.5)
+	    .attr("height", miniHeight);
+
+	function display () {
+	    if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
+	    selected =  xScaleBrush.domain()
+		.filter(function(d){
+		    return (brush.extent()[0] <= xScaleBrush(d)) && (xScaleBrush(d) <= brush.extent()[1]);
+		});
+
+	    var start;
+	    var end;
+
+	    /* Keep a minimum amount of bars on there to avoid any jank */
+	    if (selected.length > 2 ) {
+		start = selected[0];
+		end = selected[selected.length - 1] + 1;
+	    } else {
+		start = 0;
+		end = topData.length;
+	    }
+
+	    var updatedData = topData.slice(start, end);
+	    updateBars(updatedData);
+
+	}
+
+	function update (grp, data, main) {
+	    grp.selectAll("rect").data(data, function (d) {
+		return d.key;
+	    })
+		.attr("x", function (d, i) {
+		    return xScale(i);
+		})
+		.attr("width", function (d) {
+		    return xScale.bandwidth();
+		})
+		.attr("y", function (d){
+		    return main ? yScale(d.score) : 0;
+		})
+		.attr("height", function (d) {
+		    return main ? height - yScale(d.score) : miniHeight;
+		});
+	}
+
+	function enter (grp, data, main) {
+	    grp.selectAll("rect").data(data, function (d) {
+		return d.key;
+	    })
+		.enter()
+		.append("rect")
+		.attr("x", function (d, i) {
+		    return xScale(i);
+		})
+		.attr("width", function(d) {
+		    return xScale.bandwidth();
+		})
+		.attr("y", function (d){
+		    return main ? yScale(d.score) : 0;
+		})
+		.attr("height", function (d) {
+		    return main ? height - yScale(d.score) : miniHeight;
+		})
+		.attr("fill", function (d, i){
+		    var deg = d.key * 10;
+		    return "hsl(" + deg + ", 50%, 50%)";
+		})
+		.attr("opacity", function () {
+		    return 1;
+		});
+	}
+
+	function exit (grp, data) {
+	    grp.selectAll("rect").data(data, function (d) {
+		return d.key;
+	    }).exit()
+		.remove();
+	}
+
+	function updateBars (data) {
+
+	    xScale.domain(d3.range(data.length));
+	    yScale.domain([0, d3.max(data, function(d) { return d.score;})]);
+
+	    /* Update */
+	    update(barsGroup, data, true);
+
+	    /* Enter… */
+	    enter(barsGroup, data, true);
+
+	    /* Exit */
+	    exit(barsGroup, data);
+
+	    /* Call the Y axis to adjust it to the new scale */
+	    svg.select(".outer-wrapper .chart .y")
+		.transition()
+		.duration(10)
+		.call(yAxis);
+	}
+
+	updateBars(topData);
+	enter(miniGroup, topData, false);
+    }
+
+
+    render () {
+	return null;
+    }
+}
+
+class BarChart3 extends Component {
+
+    constructor(props){
+	super(props);
+    }
+    componentDidMount(){
+	var margin = {top: 20, right: 20, bottom: 110, left: 50},
+	    margin2 = {top: 430, right: 20, bottom: 30, left: 40},
+	    width = 960 - margin.left - margin.right,
+	    height = 500 - margin.top - margin.bottom,
+	    height2 = 500 - margin2.top - margin2.bottom;
+	var parseDate = d3.timeParse("%b %Y");
+	var x = d3.scaleTime().range([0, width]),
+	    x2 = d3.scaleTime().range([0, width]),
+	    y = d3.scaleLinear().range([height, 0]),
+	    y2 = d3.scaleLinear().range([height2, 0]);
+	var xAxis = d3.axisBottom(x),
+	    xAxis2 = d3.axisBottom(x2),
+	    yAxis = d3.axisLeft(y);
+	var brush = d3.brushX()
+	    .extent([[0, 0], [width, height2]])
+	    .on("brush", brushed);
+	var svg = d3.select("body").append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom);
+	svg.append("defs").append("clipPath")
+	    .attr("id", "clip")
+	    .append("rect")
+	    .attr("width", width)
+	    .attr("height", height);
+	var focus = svg.append("g")
+	    .attr("class", "focus")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	var context = svg.append("g")
+	    .attr("class", "context")
+	    .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+	let parsed_data = d3.csvParse(this.props.data, type);
+
+	function f(data)
+	{
+	    x.domain(d3.extent(data, function(d) { return d.date; }));
+	    y.domain([0, d3.max(data, function(d) { return d.price; })+200]);
+	    x2.domain(x.domain());
+	    y2.domain(y.domain());
+	    // append scatter plot to main chart area 
+	    var dots = focus.append("g");
+	    dots.attr("clip-path", "url(#clip)");
+	    dots.selectAll("dot")
+		.data(data)
+		.enter().append("circle")
+		.attr('class', 'dot')
+		.attr("r",5)
+		.style("opacity", .5)
+		.attr("cx", function(d) { return x(d.date); })
+		.attr("cy", function(d) { return y(d.price); });
+	    dots.selectAll("rect")
+		.data(data)
+		.enter().append("rect")
+		.attr('class', 'rect')
+	    	.attr("width", function(d) { return width / data.length; })
+		.attr("height", function(d) { return height - y(d.price); })
+		.style("opacity", .5)
+		.attr("x", function(d) { return x(d.date); })
+		.attr("y", function(d) { return y(d.price); });
+            
+	    focus.append("g")
+		.attr("class", "axis axis--x")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis);
+	    focus.append("g")
+		.attr("class", "axis axis--y")
+		.call(yAxis);
+	    
+	    focus.append("text")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 0 - margin.left)
+		.attr("x",0 - (height / 2))
+		.attr("dy", "1em")
+		.style("text-anchor", "middle")
+		.text("Price");  
+	    
+	    svg.append("text")             
+		.attr("transform",
+		      "translate(" + ((width + margin.right + margin.left)/2) + " ," + 
+                      (height + margin.top + margin.bottom) + ")")
+		.style("text-anchor", "middle")
+		.text("Date");
+	    
+	    // append scatter plot to brush chart area      
+	    var dots = context.append("g");
+	    dots.attr("clip-path", "url(#clip)");
+	    dots.selectAll("dot")
+		.data(data)
+		.enter().append("circle")
+		.attr('class', 'dotContext')
+		.attr("r",3)
+		.style("opacity", .5)
+		.attr("cx", function(d) { return x2(d.date); })
+		.attr("cy", function(d) { return y2(d.price); });
+	    dots.selectAll("rect")
+		.data(data)
+		.enter().append("rect")
+		.attr('class', 'rectContext')
+	    	.attr("width", function(d) { return 5; })
+		.attr("height", function(d) { return height2 - y2(d.price); })
+		.style("opacity", .5)
+		.attr("x", function(d) { return x2(d.date); })
+		.attr("y", function(d) { return y2(d.price); });
+            
+	    context.append("g")
+		.attr("class", "axis axis--x")
+		.attr("transform", "translate(0," + height2 + ")")
+		.call(xAxis2);
+	    context.append("g")
+		.attr("class", "brush")
+		.call(brush)
+		.call(brush.move, x.range());
+	};
+
+	f(parsed_data);
+	//create brush function redraw scatterplot with selection
+	function brushed() {
+	    var selection = d3.event.selection;
+	    console.log(selection);
+	    console.log(width);
+	    x.domain(selection.map(x2.invert, x2));
+	    focus.selectAll(".dot")
+		.attr("cx", function(d) { return x(d.date); })
+		.attr("cy", function(d) { return y(d.price); });
+	    focus.selectAll(".rect")
+		.attr("x", function(d) { return x(d.date); })
+		.attr("y", function(d) { return y(d.price); })
+	    	.attr("width", function(d) { return 5 * width / (selection[1]-selection[0]); })
+		.attr("height", function(d) { return height - y(d.price); });
+	    focus.select(".axis--x").call(xAxis);
+	}
+	function type(d) {
+	    d.date = parseDate(d.date);
+	    d.price = +d.price;
+	    return d;
+	}
+
+    }
+
+    render(){
+	return null;
+    }
+    
 }
 
 class ChartContainer extends Component {
@@ -167,10 +614,10 @@ class ChartContainer extends Component {
     }
 
     componentDidMount() {
-	this.timerID = setInterval(
-	    () => this.tick(),
-	    2000
-	);
+	// this.timerID = setInterval(
+	//     () => this.tick(),
+	//     2000
+	// );
     }
     
     tick() {
@@ -187,7 +634,7 @@ class ChartContainer extends Component {
     render() {
         return (
             <div>Hello world chart !!
-              <BarChartContainer data={this.state.data}/>
+              <BarChart3 data={this.state.data}/>
               It is now {this.state.date.toLocaleTimeString()}.
             </div>
         );
