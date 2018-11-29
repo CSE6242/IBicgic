5a96e4bbbe1cab007bec19396311787fb393e15a
scinart <akubeej@gmail.com>
2018-11-29 04:44:03 -0500

    Add two charts

--- src/overview/components/piechart.js
+++ src/overview/components/piechart.js
@@ -1,74 +1,82 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
+import * as dc from 'dc';
+import * as crossfilter from 'crossfilter';
+import axios from 'axios';
 
 export class PieChart extends Component {
 
     constructor(props){
         super(props);
-    }
-
-    componentDidMount() {
-	var margin = {top: 50, right: 50, bottom: 50, left: 50},
-            width = 960 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
-
-	// 1. Add the SVG to the page and employ #2
-	var svg = d3.select("body").append("svg").attr("class","piechartsvg")
-	    .attr("width", width + margin.left + margin.right)
-	    .attr("height", height + margin.top + margin.bottom);
-
-	svg.append("g").attr("class","piehere")
-	    .attr("transform", "translate(" + width/2 + "," + height/2 + ")");
-	let dataset = this.props.data;
-	// 2. Use the margin convention practice 
-	// The number of datapoints
-	let n = dataset.length;
-
-	this.update(this.props.data);
+	this.id = props.id;
     }
 
     update(data)
     {
-	let color = d3.scaleOrdinal(d3.schemeCategory10);
-        let arc = d3.arc().outerRadius(100).innerRadius(0);
-        let pie = d3.pie().sort(null);
-	let chart = d3.select(".piehere");
-        let update = chart.selectAll('.arc').data(pie(data));
-	console.log(data);
-        update.enter().append('path')
-              .attr('class', 'arc')
-              .attr('d', arc)
-              .attr("fill", (d, i) => {
-                return color(i + 1 + "");
-              })
-              .each((d) => { this._current = d; });
-        update.transition().duration(500)
-              .attrTween('d', (d) => {
-                  let a = d3.interpolate(this._current, d);
-                  this._current = a(0);
-                  return (t) => {
-                      return arc(a(t));
-                  };
-              });
-        update.exit().remove();
+	let chart = dc.pieChart(this.id);
+	let ndx = crossfilter(this.props.data);
+	let runDimension  = ndx.dimension(function(d) {return d.userType;});
+	let speedSumGroup = runDimension.group().reduceSum(function(d) {return d["sum(tripDuration)"];});
 
-	let sum=0;
-	for (var i = 0; i < data.length; sum += data[i++]);
-	let normalized_data = data.map(v => Math.round(v*100 / sum));
-        let texts = chart.selectAll('text').data(pie(normalized_data));
-        texts.exit().remove();
-        texts.enter().append('text')
-                     .text(d => Math.round(d.value) + '%')
-                     .attr('fill', '#eee')
-                     .attr('transform', d => `translate(${arc.centroid(d)})`)
-                     .style('text-anchor', 'end');
-        texts.transition().duration(500).text(d => Math.round(d.value) + '%').attr('transform', d => `translate(${arc.centroid(d)})`).style('text-anchor', 'end');;
+	chart
+	    .width(768)
+	    .height(480)
+	    .slicesCap(4)
+	    .innerRadius(100)
+	    .dimension(runDimension)
+	    .group(speedSumGroup)
+	    .legend(dc.legend())
+	    .on('pretransition', function(chart) {
+		chart.selectAll('text.pie-slice').text(function(d) {
+		    return d.data.key + ' ' + dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2*Math.PI) * 100) + '%';
+		});
+	    });
+	chart.render();
     }
     
     render () {
 	this.update(this.props.data);
 	return null;
     }
+}
+
+export class PieChartUrlUserType extends Component {
+    constructor(props){
+        super(props);
+	this.id = props.id;
+    }
+
+    update(data)
+    {
+	console.log("this.id is " + this.id);
+	console.log(data);
+	let chart = dc.pieChart(this.id);
+	let ndx = crossfilter(data);
+	let runDimension  = ndx.dimension(function(d) {return d.userType;});
+	let speedSumGroup = runDimension.group().reduceSum(function(d) {return d["sumtrip"];});
+
+	chart
+	    .width(768)
+	    .height(480)
+	    .slicesCap(4)
+	    .innerRadius(100)
+	    .dimension(runDimension)
+	    .group(speedSumGroup)
+	    .legend(dc.legend())
+	    .on('pretransition', function(chart) {
+		chart.selectAll('text.pie-slice').text(function(d) {
+		    return d.data.key + ' ' + dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2*Math.PI) * 100) + '%';
+		});
+	    });
+	chart.render();
+    }
+
+    render () {
+	axios.get(this.props.dataurl).then((res)=>{
+	    this.update(res.data);
+	});
+	return null;
+    }    
 }
 
 export default PieChart;
--- src/overview/container.js
+++ src/overview/container.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
 import BrushChart from './components/brushchart';
+import PieChart, {PieChartUrlUserType} from './components/piechart';
 import axios from 'axios';
 
 import './container.scss';
 
 class ChartContainer extends Component {
-	constructor() {
-		super();
-		this.state={
+    constructor(props) {
+	super(props);
+	this.state={
             data: []
-		}
-	}
+	};
+    }
 
-	componentDidMount() {
+    componentDidMount() {
         axios.get('https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/all-use?divide_by=day')
             .then((res) => {
-				console.log(res.data);
+		console.log(res.data);
+		this.setState({data: res.data});
             });
     }
 
     render() {
         return (
-            <div className="brush-chart-container" data={this.state.data}>
-              <BrushChart data={this.state.data}/>
-            </div>
+		<div className="brush-chart-container" data={this.state.data}>
+                <div className='brush-chart' />
+		<div className='pie-chart' id="usertype" />
+ 		<BrushChart data={this.state.data}/>
+		<PieChartUrlUserType dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=usertype" id="#usertype"/>
+		</div>
         );
     }
 }
