f030d5abeea40d86d646c99ed661fe26557b017d
Pandaice <1542752218@qq.com>
2018-11-29 03:32:58 -0500

    add charts

---
+++ src/overview/components/barchart.js
@@ -0,0 +1,14 @@
+import react, { Componnent } from 'react';
+import * as d3 from 'd3';
+
+import '../container.scss';
+
+class BarChart extends Componnent {
+    constructor() {
+        super();
+    }
+
+    render() {
+        
+    }
+}
