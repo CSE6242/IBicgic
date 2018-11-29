989fd6351f4955cffe3e4893a0add5fe0c79a790
scinart <akubeej@gmail.com>
2018-11-29 06:41:16 -0500

    working movechart

--- src/overview/container.js
+++ src/overview/container.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import BrushChart from './components/brushchart';
 import PieChart, {PieChartUrlUserType, PieChartUrlUserAge, PieChartUrlUserGender} from './components/piechart';
+import MoveChart from './components/movechart';
 import TablePopular from './components/table';
 import axios from 'axios';
 
@@ -20,6 +21,14 @@ class ChartContainer extends Component {
 		console.log(res.data);
 		this.setState({data: res.data});
             });
+
+	/*
+	  <BrushChart data={this.state.data}/>
+		<PieChartUrlUserType dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=usertype" id="#usertype"/>
+		<PieChartUrlUserAge dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=age" id="#userage"/>
+		<PieChartUrlUserGender dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=gender" id="#usergender"/>
+		<TablePopular dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=hotstation" id="#tablepopular"/>
+	*/
     }
 
     render() {
@@ -30,11 +39,9 @@ class ChartContainer extends Component {
 		<div className='pie-chart' id="userage" />
 		<div className='pie-chart' id="usergender" />
 		<div className='table' id="tablepopular" />
- 		<BrushChart data={this.state.data}/>
-		<PieChartUrlUserType dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=usertype" id="#usertype"/>
-		<PieChartUrlUserAge dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=age" id="#userage"/>
-		<PieChartUrlUserGender dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=gender" id="#usergender"/>
-		<TablePopular dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=hotstation" id="#tablepopular"/>
+		<div className='barchart' id="movechart" />
+		<div className='barchart' id="volumechart" />
+		<MoveChart dataurl='https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/all-use?divide_by=day' id1="#movechart" id2="#volumechart"/>
 		</div>
 
         );
