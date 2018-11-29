a63b96d180a188274f381b53b627615ace62be97
scinart <akubeej@gmail.com>
2018-11-29 05:37:48 -0500

    add some charts

--- src/overview/components/piechart.js
+++ src/overview/components/piechart.js
@@ -118,5 +118,45 @@ export class PieChartUrlUserAge extends Component {
     }    
 }
 
+export class PieChartUrlUserGender extends Component {
+    constructor(props){
+        super(props);
+	this.id = props.id;
+    }
+
+    update(data)
+    {
+	console.log("this.id is " + this.id);
+	console.log(data);
+	let chart = dc.pieChart(this.id);
+	let ndx = crossfilter(data);
+	let runDimension  = ndx.dimension(function(d) {return d.gender===1?"Male":"Female";});
+	let speedSumGroup = runDimension.group().reduceSum(function(d) {return d["sumtrip"];});
+
+	chart
+	    .width(768)
+	    .height(480)
+	    .slicesCap(10)
+	    .innerRadius(100)
+	    .dimension(runDimension)
+	    .group(speedSumGroup)
+	    .legend(dc.legend())
+	    .on('pretransition', function(chart) {
+		chart.selectAll('text.pie-slice').text(function(d) {
+		    return d.data.key + ' ' + dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2*Math.PI) * 100) + '%';
+		});
+	    });
+	chart.render();
+    }
+
+    render () {
+	axios.get(this.props.dataurl).then((res)=>{
+	    this.update(res.data);
+	});
+	return null;
+    }    
+}
+
+
 
 export default PieChart;
--- src/overview/container.js
+++ src/overview/container.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import BrushChart from './components/brushchart';
-import PieChart, {PieChartUrlUserType, PieChartUrlUserAge} from './components/piechart';
+import PieChart, {PieChartUrlUserType, PieChartUrlUserAge, PieChartUrlUserGender} from './components/piechart';
+import TablePopular from './components/table';
 import axios from 'axios';
 
 import './container.scss';
@@ -27,10 +28,15 @@ class ChartContainer extends Component {
                 <div className='brush-chart' />
 		<div className='pie-chart' id="usertype" />
 		<div className='pie-chart' id="userage" />
+		<div className='pie-chart' id="usergender" />
+		<div className='table' id="tablepopular" />
  		<BrushChart data={this.state.data}/>
 		<PieChartUrlUserType dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=usertype" id="#usertype"/>
 		<PieChartUrlUserAge dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=age" id="#userage"/>
+		<PieChartUrlUserGender dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=gender" id="#usergender"/>
+		<TablePopular dataurl="https://163fqjo9o1.execute-api.us-east-1.amazonaws.com/NYbike/riding-count?startDate=2016-10-01&endDate=2017-01-01&type=hotstation" id="#tablepopular"/>
 		</div>
+
         );
     }
 }
